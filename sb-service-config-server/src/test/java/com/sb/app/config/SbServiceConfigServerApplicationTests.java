65dc740388c2604612c2d16c7c1d29686e1388dd
Nicolás E. Rozo Espinosa <niko.rozo92@outlook.com>
2020-05-13 13:30:12 -0500

    Creación servidor de configuraciones

---
+++ sb-service-config-server/src/test/java/com/sb/app/config/SbServiceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbServiceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
