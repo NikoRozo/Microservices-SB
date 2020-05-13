7ae08636c554730520f1267e1ec482040b0ca4c2
Nicolás E. Rozo Espinosa <niko.rozo92@outlook.com>
2020-05-13 11:15:31 -0500

    Porto Dimanico

--- sb-service-item/src/main/java/com/sb/app/items/SbServiceItemApplication.java
+++ sb-service-item/src/main/java/com/sb/app/items/SbServiceItemApplication.java
@@ -2,10 +2,10 @@ package com.sb.app.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "service.products")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class SbServiceItemApplication {
