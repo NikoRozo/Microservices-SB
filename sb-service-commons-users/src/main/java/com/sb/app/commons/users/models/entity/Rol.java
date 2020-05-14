b9dbffdbe5d840f652a1d2249ef5fa1c4e8f3eec
Nicolás E. Rozo Espinosa <niko.rozo92@outlook.com>
2020-05-14 15:05:58 -0500

    libreria Common de usuarios

---
+++ sb-service-commons-users/src/main/java/com/sb/app/commons/users/models/entity/Rol.java
@@ -0,0 +1,54 @@
+package com.sb.app.commons.users.models.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "rols")
+public class Rol implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true, length = 30)
+	private String nombre;
+	
+	@ManyToMany(fetch = FetchType.LAZY, mappedBy = "roles")
+	private List<User> users;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2302711853556354900L;
+
+}
---
+++ sb-service-commons-users/src/test/java/com/sb/app/commons/users/SbServiceCommonsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.app.commons.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbServiceCommonsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
