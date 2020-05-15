4154edcb94852e2063b290ea9921ad50911d0fb8
Nicolás E. Rozo Espinosa <niko.rozo92@outlook.com>
2020-05-15 09:11:52 -0500

    Implementación Cors

--- sb-service-zuul-server/src/main/java/com/sb/app/zuul/oauth/ResourseServerConfig.java
+++ sb-service-zuul-server/src/main/java/com/sb/app/zuul/oauth/ResourseServerConfig.java
@@ -1,9 +1,15 @@
 package com.sb.app.zuul.oauth;
 
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -11,6 +17,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @RefreshScope
 @Configuration
@@ -34,7 +44,8 @@ public class ResourseServerConfig extends ResourceServerConfigurerAdapter {
 				"/api/items/ver/{id}/cantidad/{cantidad}", 
 				"/api/users/users/{id}").hasAnyRole("ADMIN", "USER")
 		.antMatchers("/api/products/**", "/api/items/**", "/api/users/**").hasRole("ADMIN")
-		.anyRequest().authenticated();
+		.anyRequest().authenticated()
+		.and().cors().configurationSource(corsConfigurationSource());
 		/*
 		 En esta configuración se detallan los permisos
 		.antMatchers(HttpMethod.POST, "/api/products/crear", "/api/items/crear", "/api/users/users").hasRole("ADMIN")
@@ -43,6 +54,27 @@ public class ResourseServerConfig extends ResourceServerConfigurerAdapter {
 		*/
 	}
 	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Arrays.asList("*")); // Se puede restriguir los Origenes Angular Andorid
+		corsConfig.setAllowedMethods(Arrays.asList("POST","GET", "PUT", "DELETE", "OPTIONS"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		
+		UrlBasedCorsConfigurationSource source =  new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		
+		return source;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;		
+	}
+
 	@Bean
 	public JwtTokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
