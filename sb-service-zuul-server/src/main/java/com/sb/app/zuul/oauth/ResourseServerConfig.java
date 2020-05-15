3d5ed68927d3f2b8adb7ed6a98899c34e03fa1d0
Nicolás E. Rozo Espinosa <niko.rozo92@outlook.com>
2020-05-15 08:34:09 -0500

    Variable jwtKey desde properties

--- sb-service-zuul-server/src/main/java/com/sb/app/zuul/oauth/ResourseServerConfig.java
+++ sb-service-zuul-server/src/main/java/com/sb/app/zuul/oauth/ResourseServerConfig.java
@@ -1,11 +1,9 @@
 package com.sb.app.zuul.oauth;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -19,7 +17,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class ResourseServerConfig extends ResourceServerConfigurerAdapter {
 
-	@Value("config.security.oauth.jwt.key")
+	@Value("${config.security.oauth.jwt.key}")
 	private String jwtkey;
 	
 	@Override
