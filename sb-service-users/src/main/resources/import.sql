22050be117fa90107d68c85b86d7752cfbb27d6c
Nicolás E. Rozo Espinosa <niko.rozo92@outlook.com>
2020-05-15 16:24:39 -0500

    Se implementa el servidor de configuraciones en User ademas de PostgreSQL

--- sb-service-users/src/main/resources/import.sql
+++ sb-service-users/src/main/resources/import.sql
@@ -1,9 +1,9 @@
-INSERT INTO `users` (username, password, enabled, nombre, apellido, email) VALUES ('NikoRozo','$2a$10$APrTWcd6GU3r1DafbfizKeOiobqmdYdQwwfcsleohXdRVJx1/yhva',1,'Nicolás', 'Rozo', 'niko@correo.com');
-INSERT INTO `users` (username, password, enabled, nombre, apellido, email) VALUES ('DianaA','$2a$10$JZLBUjqSCnR6LxkLnMhr/ejgca54meB.4gv6T2uFVD/MM1S7VKOOm',1,'Diana', 'Avendaño', 'diana@correo.com');
+INSERT INTO users (username, password, enabled, nombre, apellido, email) VALUES ('NikoRozo','$2a$10$APrTWcd6GU3r1DafbfizKeOiobqmdYdQwwfcsleohXdRVJx1/yhva',true,'Nicolás', 'Rozo', 'niko@correo.com');
+INSERT INTO users (username, password, enabled, nombre, apellido, email) VALUES ('DianaA','$2a$10$JZLBUjqSCnR6LxkLnMhr/ejgca54meB.4gv6T2uFVD/MM1S7VKOOm',true,'Diana', 'Avendaño', 'diana@correo.com');
 
-INSERT INTO `rols` (nombre) VALUES ('ROLE_USER');
-INSERT INTO `rols` (nombre) VALUES ('ROLE_ADMIN'); 
+INSERT INTO rols (nombre) VALUES ('ROLE_USER');
+INSERT INTO rols (nombre) VALUES ('ROLE_ADMIN'); 
 
-INSERT INTO `users_roles` (users_id, roles_id) VALUES (1, 1);
-INSERT INTO `users_roles` (users_id, roles_id) VALUES (2, 2);
-INSERT INTO `users_roles` (users_id, roles_id) VALUES (2, 1);
+INSERT INTO users_roles (users_id, roles_id) VALUES (1, 1);
+INSERT INTO users_roles (users_id, roles_id) VALUES (2, 2);
+INSERT INTO users_roles (users_id, roles_id) VALUES (2, 1);
